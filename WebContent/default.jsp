7645b302e485ad7776bf01c06ca694ef3c1b6d07
jooyoung <jooyoung@DESKTOP-KUP8SIT>
2019-01-22 16:05:59 +0900

    Servlet/JSP

--- WebContent/default.jsp
+++ WebContent/default.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+	<h1>환영합니다.</h1>
 </body>
 </html>
--- src/com/douzone/helloweb/servlet/HelloServlet.java
+++ src/com/douzone/helloweb/servlet/HelloServlet.java
@@ -14,6 +14,7 @@ public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
 		PrintWriter out = response.getWriter();
 		out.print("<h1>Hello World</h1>");
 	}
